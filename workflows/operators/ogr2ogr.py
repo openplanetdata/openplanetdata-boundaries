b2d4e0f60e2bd57e904c323b2b43621d2c0ca8c1
Laurent Pellegrino <laurent.pellegrino@gmail.com>
2026-02-10 18:48:47 +0100

    Run Docker containers as worker user to fix file permissions

--- workflows/operators/ogr2ogr.py
+++ workflows/operators/ogr2ogr.py
@@ -1,10 +1,13 @@
 """Custom Airflow operator for running ogr2ogr inside a GDAL Docker container."""
 
+import os
 import shlex
 
 from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
 from openplanetdata.airflow.defaults import DOCKER_MOUNT
+
+DOCKER_USER = f"{os.getuid()}:{os.getgid()}"
 
 
 class Ogr2OgrOperator(DockerOperator):
@@ -22,6 +25,7 @@ class Ogr2OgrOperator(DockerOperator):
         kwargs.setdefault("force_pull", True)
         kwargs.setdefault("mount_tmp_dir", False)
         kwargs.setdefault("mounts", [Mount(**DOCKER_MOUNT)])
+        kwargs.setdefault("user", DOCKER_USER)
         super().__init__(
             command=f"bash -c {shlex.quote(cmd)}",
             image=image,
